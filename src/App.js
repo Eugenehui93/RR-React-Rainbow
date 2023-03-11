202748598d9666aecdebcfe1f67bb84df5df987b
Yee Ching Hui <eugenehui93@Yees-MBP.lan>
2023-03-11 10:47:23 -0800

    fixed searchbar bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, {useState} from 'react'
 import ColorBlock from './ColorBlock';
@@ -11,32 +11,6 @@ function App() {
       <ColorBlock key={i} color={color}/>
     )
   })
-  
-  return (
-    <div className='App'>
-      {colorMap}
-    </div>
-    
-  )
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
   const addColor = (newColor) =>{
     setColors([...colors, newColor])
   }
@@ -46,6 +20,9 @@ function App() {
       <ColorForm addColor = {addColor}/>
     </div>
   )
+  
+
+ 
 }
 
 export default App;
